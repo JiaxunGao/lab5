d2fb6da7c18c7b859f0686aba1066cef900d87a2
JiaxunGao <3056751953@qq.com>
2020-11-03 13:07:40 -0500

    lan5

---
+++ app/src/main/java/com/example/lab5/Product.java
@@ -0,0 +1,38 @@
+package com.example.lab5;
+
+public class Product {
+    private String _id;
+    private String _productname;
+    private String _price;
+
+    public Product() {
+    }
+    public Product(String id, String productname, String price) {
+        _id = id;
+        _productname = productname;
+        _price = price;
+    }
+    public Product(String productname, String price) {
+        _productname = productname;
+        _price = price;
+    }
+
+    public void setId(String id) {
+        _id = id;
+    }
+    public String getId() {
+        return _id;
+    }
+    public void setProductName(String productname) {
+        _productname = productname;
+    }
+    public String getProductName() {
+        return _productname;
+    }
+    public void setPrice(String price) {
+        _price = price;
+    }
+    public String getPrice() {
+        return _price;
+    }
+}
